036b5e59acc3bdc5648ed73c6c090f80c73bcc92
isaiisaito <israelisaibarrios@gmail.com>
2019-10-14 20:56:04 -0500

    Payaqui ejercicio

---
+++ app/src/main/java/com/isai/payaqui/pantalla6.java
@@ -0,0 +1,29 @@
+package com.isai.payaqui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pantalla6 extends AppCompatActivity {
+
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla6);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Pantalla6.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
